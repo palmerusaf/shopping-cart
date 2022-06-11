ee15c45f40b4c4e0c8bc90a97962078c2829197d
Branden Palmer <brandenpalmer08@gmail.com>
2022-06-11 10:55:59 +0700

    create router, setup initial files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.scss';
+import Router from './components/router';
+import NavBar from './components/nav-bar.js';
+import Footer from './components/footer.js';
+
 
 function App() {
   return (
     <div className="App">
+      <Router/>
+      <NavBar/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/pages/sorry.js
@@ -0,0 +1,3 @@
+export default function Sorry() {
+    return <p>sorry</p>
+};
