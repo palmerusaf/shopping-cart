5e3251f5eb2cecd31f7660cea01bcd715c526097
Branden Palmer <brandenpalmer08@gmail.com>
2022-06-12 20:03:04 +0700

    refactor nav bar into header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.scss";
 import { BrowserRouter } from "react-router-dom";
 import PageRoutes from "./components/page-routes.js";
-import NavBar from "./components/nav-bar.js";
+import Header from "./components/header.js";
 import Footer from "./components/footer.js";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar />
+        <Header />
         <PageRoutes />
       </BrowserRouter>
       <Footer />
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import "../styles/header.scss";
+import React from "react";
+import NavBar from "./nav-bar";
+
+function Header() {
+  return (
+    <header>
+      <h1>Logo</h1>
+      <NavBar />
+    </header>
+  );
+}
+
+export default Header;
