0357b467e51fcf55e4452484bef5e6e0dd4ff745
Branden Palmer <brandenpalmer08@gmail.com>
2022-06-18 15:53:29 +0700

    build out footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,17 @@
+import "../styles/footer.scss";
+import GitHubLogo from "../imgs/github-logo.png";
+
 export default function Footer() {
-    return <p>footer</p>
-};
+  return (
+    <footer className="footer">
+      <a
+        href="https://github.com/palmerusaf/shopping-cart"
+        target="_blank"
+        className="footer__content"
+      >
+        <p className="footer__text">GitHub Repo</p>
+        <img className="footer__img" src={GitHubLogo} alt="GitHub Logo" />
+      </a>
+    </footer>
+  );
+}
