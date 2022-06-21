c151699f28115cec21142878c01cb1502239eb5d
Branden Palmer <brandenpalmer08@gmail.com>
2022-06-21 10:42:23 +0700

    integrate card item

--- src/components/card-area.js
+++ src/components/card-area.js
@@ -1,8 +1,9 @@
 import ProductCard from "./product-card.js";
+import products from "../data/products.json";
 
 export default function CardArea() {
-  const mapDataToCards = (data) => {
-    return <p>coming soon</p>;
-  };
-  return <div className="card-area">{mapDataToCards()}</div>;
+  const mapDataToCards = (data) =>
+    data.map((item) => <ProductCard productData={item} key={item.id} />);
+
+  return <div className="card-area">{mapDataToCards(products)}</div>;
 }
