9a24663a1102469942bb9d3f57b2191043dee4af
Branden Palmer <brandenpalmer08@gmail.com>
2022-06-23 17:51:55 +0700

    open new tab with img on img click

--- src/components/product-card.js
+++ src/components/product-card.js
@@ -7,7 +7,12 @@ export default function ProductCard(props) {
   return (
     <div className="card">
       <h2 className="card__title">{title}</h2>
-      <img className="card__img" src={image} alt={title} />
+      <img
+        onClick={() => window.open(`${image}`)}
+        className="card__img"
+        src={image}
+        alt={title}
+      />
       <p className="card__description">{description}</p>
       <label className="card__price" htmlFor="price">
         Price:{" "}
