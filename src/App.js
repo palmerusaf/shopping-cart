37b34719a226b4bca25ef1c3b4aabd72b55bd0b6
Branden Palmer <brandenpalmer08@gmail.com>
2022-07-02 19:26:06 +0700

    store ref to items when add cart button pressed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,54 @@ import { BrowserRouter } from "react-router-dom";
 import PageRoutes from "./components/page-routes.js";
 import Header from "./components/header.js";
 import Footer from "./components/footer.js";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [totalCartItems, setTotalCartItems] = useState(0);
   const [cartItems, setCartItems] = useState([]);
 
   const handleAddToCart = (event) => {
-    setTotalCartItems(totalCartItems + 1);
+    const index = getProductIndex(event);
+    if (productIsInCart(index)) {
+      increaseProductAmount(index);
+    } else {
+      setCartItems(cartItems.concat({ index: index, amount: 1 }));
+    }
+    // setTotalCartItems(totalCartItems + 1);
   };
+
+  useEffect(() => {
+    setTotalCartItems(getTotalFrom(cartItems));
+  }, [cartItems]);
+
+  function getProductIndex(event) {
+    const { parentNode } = event.target;
+    const index = [...parentNode.parentNode.children].indexOf(parentNode);
+    return index;
+  }
+
+  function productIsInCart(index) {
+    return cartItems.some((item) => item.index === index);
+  }
+
+  function increaseProductAmount(index) {
+    setCartItems(
+      cartItems.map((item) => {
+        if (item.index === index) {
+          const newAmount = item.amount + 1;
+          return { ...item, amount: newAmount };
+        }
+        return item;
+      })
+    );
+  }
+
+  function getTotalFrom(cartItems) {
+    const total = cartItems.reduce((total, { amount }) => {
+      return (total += amount);
+    }, 0);
+    return total;
+  }
 
   return (
     <div className="App">
